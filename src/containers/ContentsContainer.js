a50fda398e56c7066799011c27f0954ded985f42
안로빈 <anrobin719@gmail.com>
2019-10-18 15:10:35 +0900

    [feat] Add infinite scrolling

--- src/containers/ContentsContainer.js
+++ src/containers/ContentsContainer.js
@@ -1,30 +1,70 @@
 import React, { Component } from "react";
 import Contents from "../components/Contents";
+// import updateObject from '../lib/utility/updateObject';
 import axios from "axios";
 
 class ContentsContainer extends Component {
   state = {
-    list: []
+    list: [],
+    loadCount: 1,
+    loading: true
   };
+
+  // 페이지의 아랫부분까지 스크롤이 내려가면, 리스트 로드 이벤트를 실행합니다.
   componentDidMount() {
-    const url =
-      "https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_1.json";
+    this.loadHandler();
+
+    console.log(window.innerHeight);
+    window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scrollY + 68 >=
+        document.body.offsetHeight
+      ) {
+        this.loadHandler();
+      }
+    });
+  }
+
+  // [리스트 로드 이벤트] - 로드 할 때마다 카운트를 추가합니다.
+  loadHandler() {
+    this.setState({ loading: true });
+    const { loadCount } = this.state;
+    const url = `https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_${loadCount}.json`;
     axios
       .get(url)
       .then(res => {
-        console.log("FETCH LIST ARRAY SUCCESS", res.data);
-        this.setState({
-          list: res.data
+        console.log(`FETCH LIST ARRAY SUCCESS ${loadCount}`, res.data);
+        // setTimeout(() => {
+        this.setState(prevState => {
+          return {
+            list: prevState.list.concat(res.data),
+            loadCount: prevState.loadCount + 1,
+            loadign: false
+          };
         });
+        // }, 1000);
       })
       .catch(err => {
-        console.log("FETCH LIST ERROR", err);
+        console.log(`FETCH LIST ERROR ${loadCount}`, err);
       });
   }
 
   render() {
     const { list } = this.state;
-    return <Contents list={list} />;
+    return (
+      <>
+        <div
+          ref="myscroll"
+          style={{
+            height: "auto",
+            overflow: "auto",
+            outline: "1px solid red"
+          }}
+        >
+          <Contents list={list} />
+        </div>
+      </>
+    );
   }
 }
 
